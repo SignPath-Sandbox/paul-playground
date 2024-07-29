816e6a1e9b7f7a145e209a9cf6cbe2f18387b19a
Paul Savoie <paul@savoie.at>
2024-07-29 15:19:01 +0200

    single change (#1)
    * single change
    
    * no change
    
    * adds package.json
    
    * adds python sample
    
    * adds python header
    
    * adds another comment
    
    * adds another test case
    
    ---------
    
    Co-authored-by: Paul Savoie <paul.savoie@signpath.io>

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 /**
  * Should trigger a code scanning warning due to hard-coded secrets.
+ * See https://codeql.github.com/codeql-query-help/javascript/js-hardcoded-credentials/
  */
 let base64 = require('base-64');
 
 let url = 'http://example.org/auth';
-let username = 'user';
-let password = 'passwd';
+let username = 'username';
+let password = 'password';
 
 let headers = new Headers();
 
---
+++ src/python/sql-injection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from django.db import connection
+
+def show_user(request, username):
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM users WHERE username = %s" % username)
+		user = cursor.fetchone()
