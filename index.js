747a21f3a8c4c8c025cbda7f8e3ad3a919f62be5
Paul Savoie <paul.savoie@signpath.io>
2024-08-01 13:55:25 +0200

    critical and medium security risk now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 app.get('/', (req, res) => {
   // this line here triggers a CRITICAL security alert
   // see https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-094/CodeInjection.ql
-  // console.log(eval(req.query.js));
+  console.log(eval(req.query.js));
   res.send('Hello World')
 })
 
