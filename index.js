f72c8d18448407a368e06d650365fd49f58d1cd9
Paul Savoie <paul.savoie@signpath.io>
2024-08-01 11:50:53 +0200

    only medium now

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const port = 3000
 
 
 app.get('/', (req, res) => {
-  // this line here triggers a critical security alert
+  // this line here triggers a CRITICAL security alert
   // see https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-094/CodeInjection.ql
-  console.log(eval(req.query.js));
+  // console.log(eval(req.query.js));
   res.send('Hello World')
 })
 
-// should trigger https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-200/PrivateFileExposure.ql
+// this line triggers a MEDIUM security alert
+// see https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-200/PrivateFileExposure.ql
 app.use(express.static('node_modules/express'))
 
 app.listen(port, () => {
