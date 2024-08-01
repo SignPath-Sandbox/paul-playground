ce486b49e84ad01ea3fd834f83ba4d1c55258b20
Paul Savoie <paul.savoie@signpath.io>
2024-08-01 09:11:20 +0200

    adds another failure

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const port = 3000
 
 app.get('/', (req, res) => {
   // this line here triggers a critical security alert
+  // see https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-094/CodeInjection.ql
   console.log(eval(req.query.js));
   res.send('Hello World')
 })
+
+// should trigger https://github.com/github/codeql/blob/54ac18092daa7abb973bcd42063a01771e38e04a/javascript/ql/src/Security/CWE-200/PrivateFileExposure.ql
+app.use(express.static('node_modules/express'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
